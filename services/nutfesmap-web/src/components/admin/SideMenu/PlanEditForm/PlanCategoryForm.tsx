a39b7bdd5c2d5c1be1d36419598c541de16601b0
菅原凜々 <25.r.sugawara.nutfes@gmail.com>
2025-10-09 17:36:56 +0900

    Revert "ボトムシートのアイコンを修正"

--- services/nutfesmap-web/src/components/admin/SideMenu/PlanEditForm/PlanCategoryForm.tsx
+++ services/nutfesmap-web/src/components/admin/SideMenu/PlanEditForm/PlanCategoryForm.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import  "../FormStyle.css";
 import  "../Style.css";
-import  { Category } from "@/types/enums";
 
 type PlanCategoryFormProps = {
   value: string;
@@ -15,12 +14,12 @@ export default function PlanCategoryForm({ value, onChange }: PlanCategoryFormPr
       <label className="Label">カテゴリ</label>
       <select
         value={value}
-        onChange={(e) => onChange(e.target.value as Category)}
+        onChange={(e) => onChange(e.target.value)}
         className="categorySelect">
         <option value="">選択してください</option>
-        <option value="{Category.Food}">{Category.Food}</option>
-        <option value="{Category.Plan}">{Category.Plan}</option>
-        <option value="{Category.Child}">{Category.Child}</option>
+        <option value="food">飲食</option>
+        <option value="plan">企画</option>
+        <option value="forChild">子供向け</option>
       </select>
     </div>
   );
